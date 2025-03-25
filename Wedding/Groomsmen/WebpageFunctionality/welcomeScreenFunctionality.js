2d0ee8e46027af43bfa5b663f164bbc62e85dc76
Kendall Britton <kendallb85@gmail.com>
2025-03-24 20:56:27 -0400

    Revert layer

--- Wedding/Groomsmen/WebpageFunctionality/welcomeScreenFunctionality.js
+++ Wedding/Groomsmen/WebpageFunctionality/welcomeScreenFunctionality.js
@@ -6,7 +6,7 @@ gridContainer.style.left = '0';
 gridContainer.style.width = '100%'; // Full viewport width
 gridContainer.style.height = '100%'; // Full viewport height
 gridContainer.style.display = 'grid';
-gridContainer.style.zIndex = '1'; // Place behind all content
+gridContainer.style.zIndex = '-1'; // Place behind all content
 gridContainer.style.gap = '0'; // Remove spacing between grid cells
 gridContainer.style.transition = 'grid-template-columns 0.2s, grid-template-rows 0.2s'; // Smooth transition for grid resizing
 
