a908cabd3f8846d56acb78aa48f429ea7da45901
Kendall Britton <kendallb85@gmail.com>
2025-05-27 01:03:44 -0400

    Name update before API call implemented

--- Wedding/Groomsmen/WebpageFunctionality/profileScreen.js
+++ Wedding/Groomsmen/WebpageFunctionality/profileScreen.js
@@ -102,6 +102,31 @@ function handleCheckboxClick(selectedValue) {
     }
 }
 
+// Map to reveal names to their profile screen
+const profileMapLink = {
+    "Chris": "Chris-Profile-Link",
+    "Jalen": "Jalen-Profile-Link",
+    "Jeremiah": "Jeremiah-Profile-Link",
+    "Kaleb": "Kaleb-Profile-Link",
+    "Kayno": "Kayno-Profile-Link",
+    "Ryan": "Ryan-Profile-Link",
+    "Terrell": "Terrell-Profile-Link",
+    "Ty": "Ty-Profile-Link",
+};
+
+// Map to reveal names to their profile screen
+const profileMapName = {
+    "Chris": "Chris McClurkin",
+    "Jalen": "Jalen Goodwin",
+    "Jeremiah": "Jeremiah Stringfield",
+    "Kaleb": "Kaleb Cody",
+    "Kayno": "Kayno Speights",
+    "Ryan": "Ryan Davenport",
+    "Terrell": "Terrell Britton",
+    "Ty": "Ty Wilson",
+};
+
+
 // Function to handle the submit button
 function handleSubmit() {
 
@@ -109,9 +134,9 @@ function handleSubmit() {
 
     // TODO: Update to use maps
     // Reveal the User Profile Link when submit is clicked
-    const revealProfileLink = document.getElementById("Terrell-Profile-Link");
+    const revealProfileLink = document.getElementById(profileMapLink[localStorage.getItem("currentUserAccessName")]);
     if (revealProfileLink) {
-        revealProfileLink.textContent = "Terrell Britton (Submitted)";
+        revealProfileLink.textContent = profileMapName[localStorage.getItem("currentUserAccessName")];
     }
 }
 
